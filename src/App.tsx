65407445f6a8ae333f692cd3a3f41e691721db1c
Con Sınov <hakan.unal@radity.com>
2023-12-18 00:19:30 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,66 @@
-import { Row, Typography, Col, Image, Switch, Result } from 'antd';
-import { title1, title2, title3, paragraph1, paragraph2, paragraph3, paragraph4, paragraph5, paragraph6 } from "./language/language"
-import { useState } from 'react';
-import { GiRabbit } from "react-icons/gi";
+import { Row, Typography, Col, Image, Switch } from "antd";
+import {
+   title1,
+   title2,
+   title3,
+   paragraph1,
+   paragraph2,
+   paragraph3,
+   paragraph4,
+   paragraph5,
+   paragraph6,
+} from "./language/language";
+import { useState } from "react";
 
-const { Title, Paragraph, Text, Link } = Typography;
-
+const { Title, Paragraph } = Typography;
 
 const App = () => {
-  const [translate, setTranslate] = useState<boolean>(false)
+   const [translate, setTranslate] = useState<boolean>(false);
 
-  return (<Row justify={"center"}>
+   return (
+      <Row justify={"center"}>
+         <Switch
+            unCheckedChildren={<div>UK</div>}
+            checkedChildren={<div>EN</div>}
+            onChange={(checked) => setTranslate(checked)}
+         />
 
-    <Result
-      icon={<GiRabbit color='blue' size={100} />}
-      title="Coming soon..."
-    />
-    {/* <Switch unCheckedChildren={<div>TR</div>} checkedChildren={<div>EN</div>} onChange={(checked) => setTranslate(checked)} />
+         <Col span={24}>
+            <Title level={1}>{title1(translate)}</Title>
 
-    <Col sm={24}>
+            <Paragraph>{paragraph1(translate)}</Paragraph>
+         </Col>
+         <Col sm={4}>
+            <Image
+               width={"100%"}
+               src="https://as1.ftcdn.net/v2/jpg/03/43/21/92/1000_F_343219263_kBIXF8URy0yD4K5voCm2fAxA3ri8xrbY.jpg"
+            />
+         </Col>
+         <Col span={24}>
+            <Title level={2}>{title2(translate)}</Title>
+            <Paragraph>{paragraph2(translate)}</Paragraph>
+         </Col>
 
-      <Title level={1}>{title1(translate)}</Title>
+         <Col sm={4}>
+            <Image
+               width={"100%"}
+               src="https://img.freepik.com/premium-vector/vector-blackboard-with-tasks-sorting-scheme-kanban-board-template-blank-stickers-work_566075-71.jpg"
+            />
+         </Col>
 
-      <Paragraph >
-        {paragraph1(translate)}
-      </Paragraph>
-
-    </Col>
-    <Col sm={8}>
-      <Image width={"80%"} src='https://cdn.dribbble.com/users/52927/screenshots/2436222/remoteserviceanimation800-600.gif' />
-    </Col>
-    <Col sm={16}>
-      <Title level={2}>{title2(translate)}</Title>
-      <Paragraph>
-        {paragraph2(translate)}
-      </Paragraph>
-    </Col>
-
-    <Col sm={24}>
-      <Title level={3}>{title3(translate)}</Title>
-      <Paragraph>
-        <ul>
-          <li>
-            {paragraph3(translate)}
-          </li>
-          <li>
-            {paragraph4(translate)}
-          </li>
-          <li>
-            {paragraph5(translate)}
-          </li>
-          <li>
-            {paragraph6(translate)}
-          </li>
-        </ul>
-      </Paragraph>
-    </Col>
-
-    <Col xs={{ span: 12 }}>
-      <Image width={"100%"} src='https://miro.medium.com/v2/resize:fit:1400/1*pJRJgw1Vj-1MjhtQFZAZRA.png' />
-    </Col> */}
-
-
-  </Row>
-  )
-}
-
+         <Col span={24}>
+            <Title level={3}>{title3(translate)}</Title>
+            <Paragraph>
+               <ul>
+                  <li>{paragraph3(translate)}</li>
+                  <li>{paragraph4(translate)}</li>
+                  <li>{paragraph5(translate)}</li>
+                  <li>{paragraph6(translate)}</li>
+               </ul>
+            </Paragraph>
+         </Col>
+      </Row>
+   );
+};
 
 export default App;
--- src/language/language.ts
+++ src/language/language.ts
@@ -1,41 +1,41 @@
 import en from "./en.json"
-import tr from "./tr.json"
+import uk from "./uk.json"
 
 export const title1 = (translate: boolean) => {
-    return translate ? en.title1 : tr.title1
+    return translate ? en.title1 : uk.title1
 }
 
 export const title2 = (translate: boolean) => {
-    return translate ? en.title2 : tr.title2
+    return translate ? en.title2 : uk.title2
 }
 
 export const title3 = (translate: boolean) => {
-    return translate ? en.title3 : tr.title3
+    return translate ? en.title3 : uk.title3
 }
 
 
 
 export const paragraph1 = (translate: boolean) => {
-    return translate ? en.paragraph1 : tr.paragraph1
+    return translate ? en.paragraph1 : uk.paragraph1
 }
 
 export const paragraph2 = (translate: boolean) => {
-    return translate ? en.paragraph2 : tr.paragraph2
+    return translate ? en.paragraph2 : uk.paragraph2
 }
 
 export const paragraph3 = (translate: boolean) => {
-    return translate ? en.paragraph3 : tr.paragraph3
+    return translate ? en.paragraph3 : uk.paragraph3
 }
 
 export const paragraph4 = (translate: boolean) => {
-    return translate ? en.paragraph4 : tr.paragraph4
+    return translate ? en.paragraph4 : uk.paragraph4
 }
 
 export const paragraph5 = (translate: boolean) => {
-    return translate ? en.paragraph5 : tr.paragraph5
+    return translate ? en.paragraph5 : uk.paragraph5
 }
 
 export const paragraph6 = (translate: boolean) => {
-    return translate ? en.paragraph6 : tr.paragraph6
+    return translate ? en.paragraph6 : uk.paragraph6
 }
 
